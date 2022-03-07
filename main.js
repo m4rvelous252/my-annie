aedf82f883be640fa5bd8766572080dbfe7add9a
Trường Nghĩa <truongnghia25298@gmail.com>
2022-03-07 12:49:42 +0700

    remove music container

--- main.js
+++ main.js
@@ -62,14 +62,6 @@ if (autoRotate) {
   ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
 }
 
-// add background music
-if (bgMusicURL) {
-  document.getElementById('music-container').innerHTML += `
-<audio src="${bgMusicURL}" ${bgMusicControls? 'controls': ''} autoplay loop>    
-<p>If you are reading this, it is because your browser does not support the audio element.</p>
-</audio>
-`;
-}
 
 // setup events
 document.onpointerdown = function (e) {
